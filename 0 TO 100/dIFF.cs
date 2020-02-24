55d9efa9c215c2ce9fdeb28ad7b544ef5e08de70
Diego Marquez <59837903+diegomarquez-real@users.noreply.github.com>
2020-02-24 09:29:41 -0500

    Add files via upload

---
+++ 0 TO 100/dIFF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_TO_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int random_int;
+            string last_name;
+            Random rnd = new Random();
+            Console.WriteLine("Enter your last name: ");
+            last_name = Console.ReadLine();
+            random_int = rnd.Next(100, 1000);
+            Console.WriteLine(last_name.Substring(0, 4).ToLower() + random_int);
+            Console.ReadLine();
+        }
+    }
+}
